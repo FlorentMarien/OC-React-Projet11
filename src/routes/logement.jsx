4fdfcb57496c3ffacb3ffd317c842e767615e409
Marien Florent <Florent62350@hotmail.fr>
2023-12-06 15:11:53 +0100

    Ajout - logement information / notation

--- src/routes/logement.jsx
+++ src/routes/logement.jsx
@@ -1,8 +1,65 @@
+import "./../styles/logement.css";
+import mockeddata from "./../assets/data/logements.json";
+import icon_star from "./../assets/icon/star.svg";
+import icon_starfull from "./../assets/icon/star-full.svg";
 export default function Logement() {
+  const searchParams = new URLSearchParams(window.location.search);
+  let id = searchParams.get("id");
+
+  let focusdata = mockeddata.find((x) => x.id === id);
+  console.log(focusdata);
   return (
     <>
-      <section>
-        <h1>Logements</h1>
+      <section id="section-logement">
+        <div id="logement-carroussel">
+          <img src={focusdata.pictures[0]} />
+        </div>
+        <div className="logement-information">
+          <div>
+            <p className="logement-information-title">{focusdata.title}</p>
+            <p className="logement-information-location">
+              {focusdata.location}
+            </p>
+          </div>
+          <div className="logement-information-container-host">
+            <p>{focusdata.host.name}</p>
+            <img src={focusdata.host.picture} />
+          </div>
+        </div>
+        <div className="logement-information">
+          <div className="logement-information-tag">
+            {focusdata.tags.map((element) => {
+              return <span className="tag-span">{element}</span>;
+            })}
+          </div>
+          <div className="logement-information-rating">
+            {focusdata.rating === 0 ? (
+              <img src={icon_star} />
+            ) : (
+              <img src={icon_starfull} />
+            )}
+            {focusdata.rating < 1 ? (
+              <img src={icon_star} />
+            ) : (
+              <img src={icon_starfull} />
+            )}
+            {focusdata.rating < 2 ? (
+              <img src={icon_star} />
+            ) : (
+              <img src={icon_starfull} />
+            )}
+            {focusdata.rating < 3 ? (
+              <img src={icon_star} />
+            ) : (
+              <img src={icon_starfull} />
+            )}
+            {focusdata.rating < 4 ? (
+              <img src={icon_star} />
+            ) : (
+              <img src={icon_starfull} />
+            )}
+          </div>
+        </div>
       </section>
     </>
   );
