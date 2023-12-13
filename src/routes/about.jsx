3a25d0c11f58ece20e191280ecfe503cbb78e751
FlorentMarien <florent62350@hotmail.fr>
2023-12-14 00:29:54 +0100

    Ajout - input about

--- src/routes/about.jsx
+++ src/routes/about.jsx
@@ -1,9 +1,18 @@
 import Banner from "./../components/banner";
+import Input_list from "../components/input_list";
+import "./../styles/about.css"
 export default function About() {
   return (
     <>
       <section>
         <Banner />
+        <div className="list-input">
+          <Input_list data={["Fiabilité"]} />
+          <Input_list data={["Respect"]} />
+          <Input_list data={["Services"]} />
+          <Input_list data={["Sécurité"]} />
+        </div>
+        
       </section>
     </>
   );
